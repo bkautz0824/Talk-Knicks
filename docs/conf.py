b57555db8e1f3f65aaf227d5c1175206629818f6
Bennett-iBrain <bennettkautz@gmail.com>
2025-04-07 14:51:16 -0400

    chnages to docs

---
+++ docs/conf.py
@@ -0,0 +1,14 @@
+project = 'Talk-Knicks'
+copyright = '2025'
+author = 'Your Name'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'sphinx_rtd_theme'
---
+++ docs/index.md
@@ -0,0 +1,13 @@
+# Talk-Knicks
+
+Documentation for the Talk-Knicks project.
+
+## Overview
+
+Brief description of what your project does.
+
+## Getting Started
+
+- [Installation](installation.md)
+- [Usage](usage.md)
+- [API Reference](api.md)
